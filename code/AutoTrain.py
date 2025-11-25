b3b70c0266bdbcdb1a2f46df80717655fbd3692d
Michael <michaellee2606@gmail.com>
2025-11-25 14:36:00 +0800

    数据分析智能体 - 待处理

--- code/AutoTrain.py
+++ code/AutoTrain.py
@@ -7,6 +7,6 @@ class AutoTrain:
         self.machineLearningAgent = MachineLearningAgent(model_id, token)
 
     def start(self, data_path: str, out_directory: str):
-        self.dataAnalysisAgent.analyze()
+        # self.dataAnalysisAgent.analyze()
         self.machineLearningAgent.train(data_path, out_directory)
    

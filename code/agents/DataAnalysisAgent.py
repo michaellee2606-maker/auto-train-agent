600d755fbeb2d0e53dd36691dd651129f1a40e68
Michael <michaellee2606@gmail.com>
2025-11-25 14:28:14 +0800

    数据分析智能体 - 待处理

--- code/agents/DataAnalysisAgent.py
+++ code/agents/DataAnalysisAgent.py
@@ -6,4 +6,4 @@ class DataAnalysisAgent:
         self.agent = CodeAgent(tools=[], model=self.model, additional_authorized_imports=["pandas","h2o.automl"])
     
     def analyze(self):
-        self.agent.run("feature engineering and data analysis")
+        # self.agent.run("feature engineering and data analysis")

fbfe2d5a78bee7196d924018e1ba980dc6be610f
Michael Lee <michaellee2606@gmail.com>
2025-12-16 19:08:50 +0800

    数据分析智能体 - 初版

--- code/agents/DataAnalysisAgent.py
+++ code/agents/DataAnalysisAgent.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from smolagents import CodeAgent, InferenceClientModel
 
 class DataAnalysisAgent:
@@ -7,9 +9,32 @@ class DataAnalysisAgent:
         self.model = InferenceClientModel(model_id=model_id, token=token)
         self.agent = CodeAgent(tools=[], model=self.model, additional_authorized_imports=["pandas"])
 
+
+    def process_data(self, data: pd.DataFrame, class_column_name: str) -> pd.DataFrame:
+        class_column = data.pop(class_column_name)
+
+        numerical_features = data.select_dtypes(include=['int64', 'float64']).columns
+        categorical_features = data.select_dtypes(include=['object', 'category']).columns
+        
+        preprocessor = ColumnTransformer(
+            transformers=[
+                ('num', StandardScaler(), numerical_features),
+                ('cat', OneHotEncoder(), categorical_features)
+            ])
+        
+        features_processed = preprocessor.fit_transform(data)
+        
+        features_df = pd.DataFrame(features_processed, columns=preprocessor.get_feature_names_out())
+        
+        features_df[class_column_name] = class_column
+
+        return features_df
+
     def extract_features(self, train_data: pd.DataFrame, validate_data: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
-        # Implement feature extraction logic here TODO
-        return train_data, validate_data
+        train_features = self.process_data(train_data, class_column_name="class")
+        validate_features = self.process_data(validate_data, class_column_name="class")
+
+        return train_features, validate_features
 
     def analyze(self, train_data_path: str, validate_data_path: str) -> tuple[str, str]:
         train_features_path = os.path.dirname(train_data_path) + os.sep + "train_features.csv"

6ff059a8a320d72733d448406de509ba16ea125c
Michael Lee <michaellee2606@gmail.com>
2026-02-01 16:01:23 +0800

    模型训练智能体 - 增加网格（超参数）搜索逻辑

--- code/agents/MachineLearningAgent.py
+++ code/agents/MachineLearningAgent.py
@@ -1,6 +1,7 @@
 import os
 import h2o
 from h2o.estimators import H2OXGBoostEstimator
+from h2o.grid.grid_search import H2OGridSearch
 import matplotlib.pyplot as plt
 from matplotlib import font_manager as fm
 from smolagents import CodeAgent, InferenceClientModel
@@ -18,27 +19,44 @@ class MachineLearningAgent:
         train_features = h2o.import_file(train_feature_path)
         train_features[self.class_column] = train_features[self.class_column].asfactor()
 
-        xgboost_model = H2OXGBoostEstimator(
-            booster='gbtree',
-            backend='cpu',
-            dmatrix_type='dense',
-            tree_method='hist',
-            nfolds=5,
-            ntrees=60,
-            max_depth=6,
-            min_rows=3,
-            min_split_improvement=1e-3,
-            reg_alpha=1e-2,
-            reg_lambda=1.0,
-            learn_rate=0.05,
-            stopping_metric='AUCPR',
-            stopping_rounds=5,
-            stopping_tolerance=1e-3,
-            score_tree_interval=4,
-            seed=0
+        xgboost_params = {
+            'backend': 'auto',
+            'dmatrix_type': 'dense',
+            'tree_method': 'hist',
+            'booster': ['gbtree', 'dart'],
+            'ntrees': [60, 400],
+            'max_depth': [6, 11, 15],
+            'min_rows': [1, 5, 10],
+            'reg_alpha': [0, 0.01, 0.1],
+            'reg_lambda': [0, 0.01, 0.1],
+            'learn_rate': [0.01, 0.1]
+        }
+
+        search_criteria = {
+            'strategy': 'RandomDiscrete',
+            'max_models': 10,
+            'seed': 42,
+            'stopping_metric': 'AUCPR',
+            'stopping_tolerance': 0.001,
+            'stopping_rounds': 5
+        }
+
+        xgboost_grid = H2OGridSearch(
+            grid_id='xgboost_grid',
+            model=H2OXGBoostEstimator,
+            hyper_params=xgboost_params,
+            search_criteria=search_criteria,
+            parallelism=0
         )
 
-        xgboost_model.train(y=self.class_column, training_frame=train_features)
+        xgboost_grid.train(
+            y=self.class_column,
+            training_frame=train_features,
+            nfolds=5,
+            fold_assignment='Stratified',
+        )
+
+        xgboost_model = xgboost_grid.get_grid(sort_by='f2', decreasing=True)[0]
 
         xgboost_model.download_mojo(path=out_directory, get_genmodel_jar=True)
 
@@ -69,8 +87,8 @@ class MachineLearningAgent:
             plt.title('混淆矩阵', fontsize=14, fontweight='bold', fontproperties=self.fontProps)
 
             # Calculate Precision and Recall
-            precision = confusion_matrix.iloc[0, 1] / (confusion_matrix.iloc[0, 1] + confusion_matrix.iloc[1, 1])
-            recall = confusion_matrix.iloc[0, 1] / (confusion_matrix.iloc[0, 1] + confusion_matrix.iloc[0, 2])
+            precision = confusion_matrix.iloc[1, 2] / (confusion_matrix.iloc[0, 2] + confusion_matrix.iloc[1, 2])
+            recall = confusion_matrix.iloc[1, 2] / (confusion_matrix.iloc[1, 1] + confusion_matrix.iloc[1, 2])
 
             # Add Precision and Recall to the plot
             plt.figtext(0.5, 0.01, f'召回率: {recall:.2f}, 精确率: {precision:.2f}', wrap=True, horizontalalignment='center', fontsize=12, fontproperties=self.fontProps)
